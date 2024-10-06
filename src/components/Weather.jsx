bbafd57742678e629de471a4e6f7b55f51aed19b
vikas <rawatvikas689@gmail.com>
2024-10-06 20:20:19 +0530

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,11 +2,15 @@ import React, { useState } from "react";
 
 function Weather() {
   const [forcast, setForcast] = useState();
-
+  // "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
-
-  console.log(API_KEY)
+  let getWeatherData = async () => {
+    let respone = await fetch(`${API_URL}?q=${city}`);
+    let jsonResponse = await respone.json();
+    console.log(jsonResponse)
+  };
+  // console.log(API_KEY)
   return <></>;
 }
 
