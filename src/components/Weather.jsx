be04fff5824edba1711d7b76982406dadb3e60f3
vikas <rawatvikas689@gmail.com>
2024-10-06 20:33:04 +0530

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -2,20 +2,22 @@ import React, { useState } from "react";
 
 function Weather() {
   const [city, setCity] = useState("");
+
+  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   // "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-  // const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  // const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
-  // let getWeatherData = async () => {
-  //   let respone = await fetch(`${API_URL}q=${city}&appid=${API_KEY}`);
-  //   let jsonResponse = await respone.json();
-  //   console.log(jsonResponse);
-  // };
-  // getWeatherData();
-  // console.log(API_KEY)
-  console.log(city);
+  const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
+  let getWeatherInfo =async () => {
+      let response = await fetch(`${API_URL}?q=${city}&appid=${API_KEY}`);
+      let jsonResponse = await response.json();
+      console.log(jsonResponse);
+    // getWeatherData();
+    // console.log(API_KEY)
+  };
+  // console.log(city);
 
   let handleSearchButton = (e) => {
     e.preventDefault();
+    getWeatherInfo();
     setCity("");
   };
 
