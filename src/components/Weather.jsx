0613a52e887dd5ed1cd41c28432771ee51111a0b
vikas <rawatvikas689@gmail.com>
2024-10-06 20:25:37 +0530

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,18 +1,28 @@
 import React, { useState } from "react";
 
 function Weather() {
-  const [forcast, setForcast] = useState();
+  const [city, setCity] = useState("");
   // "https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}";
-  const API_URL = "https://api.openweathermap.org/data/2.5/weather";
-  const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
-  let getWeatherData = async () => {
-    let respone = await fetch(`${API_URL}?q=${city}`);
-    let jsonResponse = await respone.json();
-    console.log(jsonResponse);
-  };
-  getWeatherData();
+  // const API_URL = "https://api.openweathermap.org/data/2.5/weather";
+  // const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
+  // let getWeatherData = async () => {
+  //   let respone = await fetch(`${API_URL}q=${city}&appid=${API_KEY}`);
+  //   let jsonResponse = await respone.json();
+  //   console.log(jsonResponse);
+  // };
+  // getWeatherData();
   // console.log(API_KEY)
-  return <></>;
+  console.log(city)
+  return (
+    <>
+      <input
+        type="text"
+        placeholder="Enter city..."
+        value={city}
+        onChange={(e) => setCity(e.target.value)}
+      />
+    </>
+  );
 }
 
 export default Weather;
