e178946ceb252a70226e351e039bd236930c6f18
vikas <rawatvikas689@gmail.com>
2024-10-06 20:16:11 +0530

    commit

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -6,6 +6,7 @@ function Weather() {
   const API_URL = "https://api.openweathermap.org/data/2.5/weather";
   const API_KEY = "fc694d073c1f046f4ab5e5c37fedcdc4";
 
+  console.log(API_KEY)
   return <></>;
 }
 
